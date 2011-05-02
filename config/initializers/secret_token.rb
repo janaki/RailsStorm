44ccb67b8a29d4590f61a57fac810fb60c56dc0e
Theeba Soundararajan <theebasoundararajan@Theeba-Soundararajans-MacBook-Pro-4.local>
2011-05-01 22:26:44 -0700

    adding bookswap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookswap::Application.config.secret_token = '600b29d071d65205893d3d9789a81385f94cfef2811d9fdd7a10e1e0fdc1af04c6e3c9e7b63a84dc6e3051140fc793321e4d26571ee552caa1f18f223dbb2e88'
